d5341072e54b53dfc2e2b8b3f00076850e2388f8
Yuliya-1102 <60620103+Yuliya-1102@users.noreply.github.com>
2026-04-14 13:48:37 +0500

    delayFn, useFetch

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { delayFn } from "../helpers/delayFn";
+
+export const useFetch = (callback) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchFn = async (arg) => {
+    try {
+      setIsLoading(true);
+      setError("");
+
+      // искусственно создаем задержку на пару секунд
+      await delayFn();
+
+      const response = await callback(arg);
+
+      return response;
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return [fetchFn, isLoading, error];
+};
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,30 +2,47 @@ import Loader from "../../components/Loader/Loader";
 import QuestionCardList from "../../components/QuestionCardList/QuestionCardList";
 import { API_URL, API_RESOURCES } from "../../constants";
 import { useEffect, useState } from "react";
+import { useFetch } from "../../hooks/useFetch";
 
 const HomePage = () => {
   const [questions, setQuestions] = useState([]);
 
-  const fetchQuestions = async () => {
-    try {
-      const response = await fetch(`${API_URL}/${API_RESOURCES}`, {
-        cache: "no-store", // отключаем кэш браузера
-        headers: { "Cache-Control": "no-cache" }, // явно говорим не использовать кэш
-      });
-      const questions = await response.json();
-      setQuestions(questions);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const [getQuestions, isLoading, error] = useFetch(async (url) => {
+    const response = await fetch(`${API_URL}/${url}`, {
+      cache: "no-store", // отключаем кэш браузера
+      headers: { "Cache-Control": "no-cache" }, // явно говорим не использовать кэш
+    });
+    const questions = await response.json();
+    setQuestions(questions);
+  });
+
+  // const fetchQuestions = async () => {
+  //   try {
+  //     setIsLoading(true);
+
+  //     await delayFn();
+
+  //     const response = await fetch(`${API_URL}/${API_RESOURCES}`, {
+  //       cache: "no-store", // отключаем кэш браузера
+  //       headers: { "Cache-Control": "no-cache" }, // явно говорим не использовать кэш
+  //     });
+  //     const questions = await response.json();
+  //     setQuestions(questions);
+  //   } catch (error) {
+  //     console.error(error);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
 
   useEffect(() => {
-    fetchQuestions();
+    getQuestions("react1");
   }, []);
 
   return (
     <>
-      <Loader />
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
       <QuestionCardList cards={questions} />
     </>
   );
