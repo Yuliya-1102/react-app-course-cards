a244415b47ef1e9bf359255d522056d4f34ce7d8
Yuliya-1102 <60620103+Yuliya-1102@users.noreply.github.com>
2026-04-15 19:40:01 +0500

    sort logic

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import Loader from "../../components/Loader/Loader";
 import QuestionCardList from "../../components/QuestionCardList/QuestionCardList";
@@ -11,9 +11,14 @@ import { SearchInput } from "../../components/SearchInput";
 const HomePage = () => {
   const [questions, setQuestions] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [sortSelectValue, setSortSelectValue] = useState("");
 
+  // собственный хук -------------------------------
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
-    const response = await fetch(`${API_URL}/${API_RESOURCES}`);
+    const response = await fetch(`${API_URL}/${url}`, {
+      cache: "no-store", // отключаем кэш браузера
+      headers: { "Cache-Control": "no-cache" } // явно говорим не использовать кэш
+    });
 
     if (!response.ok) {
       throw new Error(`Could not fetch ${url}, status ${response.status}`);
@@ -24,24 +29,44 @@ const HomePage = () => {
     setQuestions(questions);
     return questions;
   });
+  // END ----------------------------
+
+  // ФИЛЬТРАЦИЯ завернули в useMemo,
+  const cards = useMemo(() => {
+    return questions.filter((d) => d.question.toLowerCase().includes(searchValue.trim().toLowerCase()));
+  }, [questions, searchValue]);
 
   useEffect(() => {
-    getQuestions(API_RESOURCES);
-  }, []);
+    getQuestions(`${API_RESOURCES}?${sortSelectValue}`);
+  }, [sortSelectValue]);
 
   const onSearchChangeHandler = (e) => {
     setSearchValue(e.target.value);
+  };
+
+  const onSortChangeHandler = (e) => {
+    setSortSelectValue(e.target.value);
   };
 
   return (
     <>
       <div className={cls.controlsContainer}>
         <SearchInput value={searchValue} onChange={onSearchChangeHandler} />
+
+        <select value={sortSelectValue} onChange={onSortChangeHandler} className={cls.select}>
+          <option value="">sort by</option>
+          <hr />
+          <option value="_sort=level&_order=asc">level ABS</option>
+          <option value="_sort=level&_order=desc">level DESC</option>
+          <option value="_sort=completed&_order=asc">completed ABS</option>
+          <option value="_sort=completed&_order=desc">completed DESC</option>
+        </select>
       </div>
 
       {isLoading && <Loader />}
       {error && <p>{error}</p>}
-      <QuestionCardList cards={questions} />
+      {cards.length === 0 && <p className={cls.noCardsInfo}>No cards</p>}
+      <QuestionCardList cards={cards} />
     </>
   );
 };
