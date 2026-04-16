8b77e088ce473e82a3c0a77b39d5520f7894214b
Yuliya-1102 <60620103+Yuliya-1102@users.noreply.github.com>
2026-04-16 15:27:42 +0500

    count select

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 export const API_URL = "http://localhost:8801";
 export const API_RESOURCES = "react";
-export const DEFAULT_PER_PAGE = 10;
+
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useRef, useState } from "react";
 
 import Loader from "../../components/Loader/Loader";
 import QuestionCardList from "../../components/QuestionCardList/QuestionCardList";
-import { API_URL, API_RESOURCES, DEFAULT_PER_PAGE } from "../../constants";
+import { API_URL, API_RESOURCES } from "../../constants";
 import { useFetch } from "../../hooks/useFetch";
 
 import cls from "./HomePage.module.css";
@@ -13,7 +13,8 @@ const HomePage = () => {
   const [questions, setQuestions] = useState({});
   const [searchValue, setSearchValue] = useState(""); // ФИЛЬТРАЦИЯ на фронте, без серверного запроса
   const [sortSelectValue, setSortSelectValue] = useState(""); // СОРТИРОВКА С ЗАПРОСОМ НА СЕРВЕР
-  const [searchParams, setSearchParams] = useState(`?_page=1&_per_page=${DEFAULT_PER_PAGE}`); // ПАГИНАЦИЯ ПО СТР С ЗАПРОСОМ НА СЕРВЕР
+  const [countSelectValue, setCountSelectValue] = useState(""); // СОРТИРОВКА по количеству на страницу
+  const [searchParams, setSearchParams] = useState(`?_page=1&_per_page=${countSelectValue}`); // ПАГИНАЦИЯ ПО СТР С ЗАПРОСОМ НА СЕРВЕР
   const controlsContainerRef = useRef(""); // для скролла вверх
 
   // собственный хук -------------------------------
@@ -59,7 +60,14 @@ const HomePage = () => {
   const onSortChangeHandler = (e) => {
     setSortSelectValue(e.target.value);
 
-    setSearchParams(`?_page=1&_per_page=${DEFAULT_PER_PAGE}&${e.target.value}`);
+    setSearchParams(`?_page=1&_per_page=${countSelectValue}&${e.target.value}`);
+  };
+
+  // переключение количества карточек на странице
+  const onCountSelectChangeHandler = (e) => {
+    setCountSelectValue(e.target.value);
+
+    setSearchParams(`?_page=1&_per_page=${e.target.value}&${sortSelectValue}`);
   };
 
   // создаем кнопки
@@ -74,7 +82,7 @@ const HomePage = () => {
   // по клику на кнопку меняем страницу
   const paginationHandler = (e) => {
     if (e.target.tagName === "BUTTON") {
-      setSearchParams(`?_page=${e.target.textContent}&_per_page=${DEFAULT_PER_PAGE}&${sortSelectValue}`);
+      setSearchParams(`?_page=${e.target.textContent}&_per_page=${countSelectValue}&${sortSelectValue}`);
 
       controlsContainerRef.current.scrollIntoView({ behavior: "smooth" });
     }
@@ -95,6 +103,18 @@ const HomePage = () => {
           <option value="_sort=completed">completed ABS</option>
           <option value="_sort=-completed">completed DESC</option>
         </select>
+
+        <select value={countSelectValue} onChange={onCountSelectChangeHandler} className={cls.select}>
+          <option value="" disabled>
+            count
+          </option>
+          <hr />
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
       </div>
 
       {isLoading && <Loader />}
@@ -105,37 +125,20 @@ const HomePage = () => {
       {cards.length === 0 ? (
         <p className={cls.noCardsInfo}>No cards</p>
       ) : (
-        <div className={cls.paginationContainer} onClick={paginationHandler}>
-          {pagination.map((btn) => {
-            return (
-              <Button key={btn} isActive={btn === getActivePageNumber()}>
-                {btn}
-              </Button>
-            );
-          })}
-        </div>
+        pagination > 1 && (
+          <div className={cls.paginationContainer} onClick={paginationHandler}>
+            {pagination.map((btn) => {
+              return (
+                <Button key={btn} isActive={btn === getActivePageNumber()}>
+                  {btn}
+                </Button>
+              );
+            })}
+          </div>
+        )
       )}
     </>
   );
 };
 
 export default HomePage;
-
-// const fetchQuestions = async () => {
-//   try {
-//     setIsLoading(true);
-
-//     await delayFn();
-
-//     const response = await fetch(`${API_URL}/${API_RESOURCES}`, {
-//       cache: "no-store", // отключаем кэш браузера
-//       headers: { "Cache-Control": "no-cache" }, // явно говорим не использовать кэш
-//     });
-//     const questions = await response.json();
-//     setQuestions(questions);
-//   } catch (error) {
-//     console.error(error);
-//   } finally {
-//     setIsLoading(false);
-//   }
-// };
