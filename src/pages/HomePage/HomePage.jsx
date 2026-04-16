a42b329f489f756af2dc7bb10d986d15d3fb2735
Yuliya-1102 <60620103+Yuliya-1102@users.noreply.github.com>
2026-04-16 12:03:42 +0500

    change json-server, fix sort

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
   const [getQuestions, isLoading, error] = useFetch(async (url) => {
     const response = await fetch(`${API_URL}/${url}`, {
       cache: "no-store", // отключаем кэш браузера
-      headers: { "Cache-Control": "no-cache" } // явно говорим не использовать кэш
+      headers: { "Cache-Control": "no-cache" }, // явно говорим не использовать кэш
     });
 
     if (!response.ok) {
@@ -36,8 +36,10 @@ const HomePage = () => {
     return questions.filter((d) => d.question.toLowerCase().includes(searchValue.trim().toLowerCase()));
   }, [questions, searchValue]);
 
+  // СОРТИРОВКА ?_sort=level&_order=asc (json-server)
   useEffect(() => {
     getQuestions(`${API_RESOURCES}?${sortSelectValue}`);
+    // getQuestions(`react?_page=1&_per_page=5`);
   }, [sortSelectValue]);
 
   const onSearchChangeHandler = (e) => {
@@ -56,10 +58,10 @@ const HomePage = () => {
         <select value={sortSelectValue} onChange={onSortChangeHandler} className={cls.select}>
           <option value="">sort by</option>
           <hr />
-          <option value="_sort=level&_order=asc">level ABS</option>
-          <option value="_sort=level&_order=desc">level DESC</option>
-          <option value="_sort=completed&_order=asc">completed ABS</option>
-          <option value="_sort=completed&_order=desc">completed DESC</option>
+          <option value="_sort=level">level ABS</option>
+          <option value="_sort=-level">level DESC</option>
+          <option value="_sort=completed">completed ABS</option>
+          <option value="_sort=-completed">completed DESC</option>
         </select>
       </div>
 
