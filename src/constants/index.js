b66972e1cd9a5a24dffc96766283dd98013f6d75
Yuliya-1102 <60620103+Yuliya-1102@users.noreply.github.com>
2026-04-27 15:04:27 +0500

    edded .env

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
-export const API_URL = "http://localhost:8801";
+// export const API_URL = "http://localhost:8801";
+export const API_URL = import.meta.env.VITE_SERVER_URL;
 export const API_RESOURCES = "react";
 
 // localStorage переменные
